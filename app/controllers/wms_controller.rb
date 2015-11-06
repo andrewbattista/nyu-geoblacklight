4e72ea953a3752643088d0f0c4e1086df287e27b
Ubuntu <sgbalogh@gmail.com>
2015-11-06 20:33:45 +0000

    initial

---
+++ app/controllers/wms_controller.rb
@@ -0,0 +1,15 @@
+class WmsController < ApplicationController
+
+  before_action :format_url
+  def handle
+    response = Geoblacklight::WmsLayer.new(params).get_feature_info
+
+    respond_to do |format|
+      format.json { render json: response }
+    end
+  end
+
+  def format_url
+    params['URL'] = Settings.RESTRICTED_URL if params['URL'].include? Settings.PROXY_URL
+ end
+end
