463fa3e4c3dc67abf701fe9d0c4cc66629d5a5ef
Ubuntu <sgbalogh@gmail.com>
2015-11-13 16:49:12 +0000

    configs mail to use sendmail, changes 404

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,20 +64,27 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-	config.action_mailer.default_options = {
-	:from => 'no-reply@nyusdr.net'
-}
 
-	config.action_mailer.delivery_method = :smtp
-	# SMTP settings for gmail
-	config.action_mailer.smtp_settings = {
-	 :address              => "smtp.gmail.com",
-	 :port                 => 587,
-	 :user_name            => ENV['gmail_username'],
-	 :password             => ENV['gmail_password'],
-	 :authentication       => "plain",
-	:enable_starttls_auto => true
-	}
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@geoblacklight.nyusdr.net'}
+
+	# config.action_mailer.delivery_method = :smtp
+	# # SMTP settings for gmail
+	# config.action_mailer.smtp_settings = {
+	#  :address              => "smtp.gmail.com",
+	#  :port                 => 587,
+	#  :user_name            => ENV['gmail_username'],
+	#  :password             => ENV['gmail_password'],
+	#  :authentication       => "plain",
+	# :enable_starttls_auto => true
+	# }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
