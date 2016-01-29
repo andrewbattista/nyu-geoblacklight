60310e176c358644e32a2f768f8726afe4b08a23
Ubuntu <sgbalogh@gmail.com>
2016-01-29 18:13:01 +0000

    changes link for metadata submissions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@geoblacklight.nyusdr.net'}
+  config.action_mailer.default_options = {from: 'no-reply@geo.nyu.edu'}
 
 	# config.action_mailer.delivery_method = :smtp
 	# # SMTP settings for gmail
